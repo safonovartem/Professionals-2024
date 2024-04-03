6c7f9e1e6a533aebea207c8934249656b7826372
safonovartem <safonovartem829@gmail.com>
2024-04-03 13:29:40 +0300

    Пытаюсь сделать модуль б

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import io
+import csv
 import os
 import shutil
 #import tensforflow as tf
@@ -16,120 +17,193 @@ from razdel import tokenize, sentenize
 from pymorphy2 import MorphAnalyzer
 from navec import Navec
 import slovnet
+from sklearn.feature_extraction.text import CountVectorizer
+from tqdm import tqdm
 
 nltk.download("stopwords", quiet=True) # поддерживает удаление стоп-слов
 nltk.download('punkt', quiet=True) # делит текст на список предложений
 nltk.download('wordnet', quiet=True) # проводит лемматизацию
+pd.set_option('display.expand_frame_repr', False)
 
 # Добавляю датасет Data
-data = pd.read_csv('data.csv')
-parse_dates = ['Text']
-#print(data)
+# with open ('data.csv', newline = '') as csvfile:
+#     reader = csv.DictReader(csvfile, delimiter = ";")
+#     for row in reader:
+#         print(row['Text'], '|' , row['Category'])
+# data = pd.read_csv('data.csv')
+# parse_dates = ['Text']
+# print(data)
+#print(data[3:4])
 
 # Добавляю датасет Data
-demo = pd.read_csv('outer_msgs_demo.csv')
-parse_dates = ['Text']
+# with open ('outer_msgs_demo.csv', newline = '') as csvfile:
+#     reader = csv.DictReader(csvfile, delimiter = ";")
+#     for row in reader:
+#         print(row['Text'], '|' , row['Category'])
+# demo = pd.read_csv('outer_msgs_demo.csv')
+# parse_dates = ['Text']
+# demo = demo['Text']
 #print(demo)
 
 # Преобрзование в строку data
-x = re. sub (' [^a-zA-Z] ', '', str (data))
-#print(x)
+#x = re. sub (' [^a-zA-Z] ', '', str (data))
+# print(x[52:110])
 
-# Преобрзование в строку demo
-y = re.sub(' [^a-zA-Z] ', '', str (demo))
-#print(y)
+# # Преобрзование в строку demo
+# y = re.sub(' [^a-zA-Z] ', '', str (demo))
+# #print(y)
 
-datasets = y + x
+#datasets = x
 #print(datasets)
 
-# Удаление пунктуации
-without_punct_words= datasets.translate(str.maketrans('', '', string.punctuation))
-#print(without_punct_words)
+# # # Удаление пунктуации
+# without_punct_words= datasets.translate(str.maketrans('', '', string.punctuation))
+# #print(without_punct_words)
 
 # Удаление чисел
-numbers = r'[0-9]'
-no_numbers_words = re.sub(numbers, '', without_punct_words)
+# numbers = r'[0-9]'
+# no_numbers_words = re.sub(numbers, '', datasets)
 #print(no_numbers_words)
-#
+
 # # Преобразование в строки в нижний регистр
 # low_words = no_numbers_words.lower()
 # #print(low_words)
 
 #Токенизация
-word_tokenize = word_tokenize(no_numbers_words)
-#print(word_tokenize)
+# word_tokenize = word_tokenize(no_numbers_words)
+# print(word_tokenize)
+# print(word_tokenize[10:225])
 
-# Лемматизация
-stemmer = SnowballStemmer("russian")
-tokens = word_tokenize
-lemmatized_words = [stemmer.stem(word) for word in tokens]
-#print(lemmatized_words)
+# # Лемматизация
+# stemmer = SnowballStemmer("russian")
+# tokens = word_tokenize
+# lemmatized_words = [stemmer.stem(word) for word in tokens]
+# print(lemmatized_words)
 
-# Удаление стоп слов
-stop_words = set(stopwords.words('russian'))
-filtered_tokens = [word for word in lemmatized_words if word not in stop_words]
-# new_filtered_tokens = set(filtered_tokens)
-print(filtered_tokens)
-
-
-# Морфологический анализ
-def segment_text(doc: str|dict) -> dict:
-    if isinstance(doc, str):
-        doc = {"text": doc}
-
-    doc["tokens"] = []
-
-    for sent in sentenize(doc["text"]):
-        doc["tokens"].append([_.text for _ in tokenize(sent.text)])
-
-    return doc
-
-analyzer = MorphAnalyzer()
-stop_words = stopwords.words("russian")
-pos = {'NOUN','ADJF','ADJS','VERB','INFN','PRTF','PRTS'}
-
-def extract_candidates(doc: dict, stop_words: list = stop_words, pos: set = pos) -> dict:
-    res = set()
-    for sent in doc["tokens"]:
-        for token in sent:
-            if token in stop_words or token in res:
-                continue
-
-            parsed = analyzer.parse(token)[0]
-            if parsed.tag.POS not in pos:
-                continue
-            res.add(token)
-    doc["candidates"] = res
-
-    return doc
+# # Удаление стоп слов
+# stop_words = set(stopwords.words('russian'))
+# filtered_tokens = [word for word in lemmatized_words if word not in stop_words]
+# # new_filtered_tokens = set(filtered_tokens)
+# #print(filtered_tokens)
 
 
-# Синтактический анализ
-navec = Navec.load("navec_news_v1_1B_250K_300d_100q.tar")
-syntax = slovnet.Syntax.load("slovnet_syntax_news_v1.tar")
-syntax.navec(navec)
+# # Морфологический анализ
+# def segment_text(doc: str|dict) -> dict:
+#     if isinstance(doc, str):
+#         doc = {"text": doc}
+#
+#     doc["tokens"] = []
+#
+#     for sent in sentenize(doc["text"]):
+#         doc["tokens"].append([_.text for _ in tokenize(sent.text)])
+#
+#     return doc
+#
+# analyzer = MorphAnalyzer()
+# stop_words = stopwords.words("russian")
+# pos = {'NOUN','ADJF','ADJS','VERB','INFN','PRTF','PRTS'}
+#
+# def extract_candidates(doc: dict, stop_words: list = stop_words, pos: set = pos) -> dict:
+#     res = set()
+#     for sent in doc["tokens"]:
+#         for token in sent:
+#             if token in stop_words or token in res:
+#                 continue
+#
+#             parsed = analyzer.parse(token)[0]
+#             if parsed.tag.POS not in pos:
+#                 continue
+#             res.add(token)
+#     doc["candidates"] = res
+#
+#     return doc
+#
+#
+# # Синтактический анализ
+# navec = Navec.load("navec_news_v1_1B_250K_300d_100q.tar")
+# syntax = slovnet.Syntax.load("slovnet_syntax_news_v1.tar")
+# syntax.navec(navec)
+#
+# def syntax_collocations(doc: dict, sytax: slovnet.api.Syntax = syntax) -> dict:
+#     syntax_colloc = []
+#     for sent in doc["tokens"]:
+#         syntax_markup = syntax(sent)
+#
+#         sent_word_id = {}
+#         for token in syntax_markup.tokens:
+#             sent_word_id[token.id] = token.text
+#
+#         for token in syntax_markup.tokens:
+#             if token.head_id != '0' and token.text in doc["candidates"]:
+#                 syntax_colloc.append(sent_word_id[token.head_id] + ' ' + token.text)
+#     doc["collocations"] = set(syntax_colloc)
+#
+#     return doc
+#
+# doc = segment_text(datasets)
+# doc = extract_candidates(doc)
+# doc = syntax_collocations(doc)
+# a = list(doc["collocations"])
+# #print(a)
+#
+# New_dataset = a + filtered_tokens
+# #print(New_dataset)
+#
+# #New_dataset.remove('error')
+# #print(New_dataset)
+#
+# # Bag of words
+# #
+# # # создать словарный запас
+# # vocab = set()
+# #
+# # # создать модель мешка слов
+# # bow_model = []
+# #
+# # for text in no_numbers_words:
+# #     # создать словарь для хранения количества слов
+# #     word_counts = {}
+# #
+# #     # токенизация
+# #     tokens = nltk.word_tokenize(text)
+# #
+# #     # обновляем список
+# #     vocab.update(tokens)
+# #
+# #     # подсчитать количество вхождений каждого слова
+# #     for word in tokens:
+# #         if word in word_counts:
+# #             word_counts[word] += 1
+# #         else:
+# #             word_counts[word] = 1
+# #
+# #     # add the word counts to the bag-of-words model
+# #     bow_model.append(word_counts)
+# #
+# # # распечатать словарь
+# # print(vocab)
+# #
+# # # распечатать количество слов для первого текстового документа
+# # print(bow_model[3])
+#
+# # Кластеизация
+#
+# Category = ['tennis', 'autosport', ';autosport',]
 
-def syntax_collocations(doc: dict, sytax: slovnet.api.Syntax = syntax) -> dict:
-    syntax_colloc = []
-    for sent in doc["tokens"]:
-        syntax_markup = syntax(sent)
 
-        sent_word_id = {}
-        for token in syntax_markup.tokens:
-            sent_word_id[token.id] = token.text
+#Добавляю test
+# with open ('test (1).csv', newline = '') as csvfile:
+#     reader = csv.DictReader(csvfile)
+#     for row in reader:
+#         print(row['text'], '|' , row['category'])
 
-        for token in syntax_markup.tokens:
-            if token.head_id != '0' and token.text in doc["candidates"]:
-                syntax_colloc.append(sent_word_id[token.head_id] + ' ' + token.text)
-    doc["collocations"] = set(syntax_colloc)
+test = pd.read_csv('test (1).csv')
+# темы текстов из которых буду брать текста
+topic = ['football', 'volleyball', 'autosport', 'hockey', 'tennis', 'esport', 'basketball', 'martial_arts', 'winter_sport', 'athletics', 'extreme', 'motosport', 'boardgames']
+number_of_text = 2 # по 2 текста на тему
 
-    return doc
+df_res = pd.DataFrame()
 
-doc = segment_text(datasets)
-doc = extract_candidates(doc)
-doc = syntax_collocations(doc)
-a = list(doc["collocations"])
-#print(a)
-
-New_dataset = a + filtered_tokens
-print(New_dataset)
+for topic in tqdm(topic):
+    df_topic = test[test['category'] == topic][:number_of_text]
+    df_res = pd.concat(df_topic, pd.DataFrame([df_topic]), ignore_index=True)
